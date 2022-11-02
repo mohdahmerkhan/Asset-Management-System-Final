7edf124cf5756ae5126cee807c665449a102fbcc
Mohd Ahmer Khan <mohd.ahmerkhan@nissanmotor.com>
2022-11-02 17:34:15 +0530

    Uploaded Angular Files

--- Asset-Management-System-Angular/src/app/app.component.ts
+++ Asset-Management-System-Angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Asset-Management-System-Angular';
+  title = 'Loan-Management-System';
 }
