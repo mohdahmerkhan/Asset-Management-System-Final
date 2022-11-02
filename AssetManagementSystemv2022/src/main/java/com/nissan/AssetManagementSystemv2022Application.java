eba0fd8133b25d984da225771df813cc9eab4b3d
divya <divyamadireddy2000@gmail.com>
2022-11-02 09:27:00 +0530

    AssetDefintion RESTAPI

---
+++ AssetManagementSystemv2022/src/main/java/com/nissan/AssetManagementSystemv2022Application.java
@@ -0,0 +1,14 @@
+package com.nissan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagementSystemv2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagementSystemv2022Application.class, args);
+		System.out.println("welcome to Asset Management System");
+	}
+
+}
